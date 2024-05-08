8c3526214fb0ecc020d48b2231c4a4365f726290
katelim215 <katelimosnero215@gmail.com>
2024-05-08 21:52:42 +0800

    edited number format

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -50,7 +50,7 @@ const ContactForm = ({ contacts, setContacts }) => {
                     name="number"
                     value={number}
                     onChange={handleNumberChange}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    pattern="^[0-9+]+$"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
                   />
